c8678dfcbb55cef201943668addcc44195cccd00
minho <pmh9038@naver.com>
2026-01-28 11:50:12 +0900

    유효점수 구하기 및 코드 수정

--- 연습문제7.c
+++ 연습문제7.c
@@ -4,23 +4,44 @@
 int main(void) {
 
 	int score[5];
-	int sum = 0;
-	int max, min;		//최고점, 최저점
+	int sum = 0;		//점수의 총합
+	int max;			//최고점
+	int min;			//최저점
 	double avg;			//평균
 
 	printf("5명 심사위원의 점수 입력: ");
 	for (int i = 0; i < 5; i++) {
 		scanf("%d", &score[i]);
 	}
-
-
-
+	
+	// 유효점수 구하기
+	max = score[0];
+	min = score[0];
+	for (int i = 0; i < 5; i++) {
+		sum += score[i];
+		if (score[i] > max) {
+			max = score[i];
+		}
+		if (score[i] < min) {
+			min = score[i];
+		}
+	}
+	
+	// 유효점수 출력하기
+	printf("유효점수: ");
+	for (int i = 0; i < 5; i++) {
+		if (score[i] == max) {		//점수가 최고점(max)이면 건너뛰기
+			continue;
+		}
+		if (score[i] == min) {		//점수가 최고점(max)이면 건너뛰기
+			continue;
+		}
+		printf("%d ",score[i]);		//건너뛴 값 빼고 출력
+	}
 
 	//평균 구하기
-	avg = (sum - max - min) / 3.0;
-
-	printf("유효점수: ");
-	printf("평균: %.1f", avg);
+	avg = (sum - max - min) / 3.0;	//(총합-최고점-최저점) / 3.0 = 평균
+	printf("평균: %.1f", avg);		//소수점 첫번째 자리까지 표시
 	return 0;
 }
 
