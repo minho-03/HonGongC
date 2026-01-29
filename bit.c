79cefeded0cedfc7b2584bd9a3edb041ebbcfeac
minho <pmh9038@naver.com>
2026-01-29 10:54:44 +0900

    버퍼 지우기

---
+++ bit.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+/*
+int a = 12;
+int b = 10;
+
+a& b =
+a | b =
+a ^ b =
+*/
+
+//int x = 1;
+//
+//x << 4
+//16 >> 2
+
+//unsigned char value = ob10100101;
+//
+//value |= (1 << 1);
+//value &= ~(1 << 2);
